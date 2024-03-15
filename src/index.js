3606edf9e3f663a0f11590aca37f28e780b02d3f
RyanGuinchard <ryanguinchard9@hotmail.com>
2024-03-15 20:58:10 -0230

    created products router and test cases

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
-
 const PORT = 3000;
+
+// Add route files
+const productsRouter = require("./routes/products");
+
+//Mount the route file
+app.use('/products', productsRouter);
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
